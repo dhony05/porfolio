721deac16af8cc85ac95923ad0803a3ff4a7a55d
Donelys Familia <donelysprivate@gmail.com>
2020-01-22 09:40:28 -0500

    Updates

---
+++ precache-manifest.fff7f79f32f074ab65ad8e692a0ae9e1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eef4a6f5c4d932757c5968583163d02",
+    "url": "/porfolio/index.html"
+  },
+  {
+    "revision": "b1aa136975e52425285c",
+    "url": "/porfolio/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "2376072f8db12e26c8ab",
+    "url": "/porfolio/static/css/main.7d5d93fa.chunk.css"
+  },
+  {
+    "revision": "b1aa136975e52425285c",
+    "url": "/porfolio/static/js/2.f944f2e6.chunk.js"
+  },
+  {
+    "revision": "c7abd4a823ee85afac2f05677368b7de",
+    "url": "/porfolio/static/js/2.f944f2e6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2376072f8db12e26c8ab",
+    "url": "/porfolio/static/js/main.50ede93b.chunk.js"
+  },
+  {
+    "revision": "df7e8f1c5ba2791f455c",
+    "url": "/porfolio/static/js/runtime-main.57e73628.js"
+  },
+  {
+    "revision": "a58eeebdb1a48a2b6e3b27bdf9f7d84b",
+    "url": "/porfolio/static/media/codewars.a58eeebd.svg"
+  }
+]);
