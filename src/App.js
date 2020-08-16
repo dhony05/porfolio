92af2724cdde65ef4af58e660a71895a4bd8ffa5
Donelys Familia <donelysprivate@gmail.com>
2020-08-16 00:05:28 -0400

    adding border to main pic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-import { Layout } from "react-mdl";
 import "./App.css";
 import NavComponent from "./components/navComponent";
 import {HashRouter} from "react-router-dom";
@@ -8,13 +7,11 @@ import {HashRouter} from "react-router-dom";
 function App() {
   return (
     <HashRouter basename="/">
-    <div className="demo-big-content">
-      <Layout>
+  
         <NavComponent>
 
         </NavComponent>
-      </Layout>
-    </div>
+      
     </HashRouter>
   );
 }
--- src/components/navComponent.js
+++ src/components/navComponent.js
@@ -1,89 +1,81 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Main from "./main";
-import { Layout,Header, Navigation, Drawer,Content} from "react-mdl";
+import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
 import { Link } from "react-router-dom";
 
-
 class NavComponent extends Component {
-    render() {
-        let width = window.innerWidth;
-        if(width>700){
-        return ( 
-           
-                <div className="demo-big-content">
-                <Layout>
-                <Header
-          className="header-color"
-          title={
-            <Link style={{ textDecoration: "none", color: "white" }} to="/">
-               Profile{" "}
-            </Link>
-          }
-          scroll
-        >
-          <Navigation>
-          <Link to="/aboutme">About me</Link>
-            <a
-              href="https://drive.google.com/file/d/1jYPWM_KJPWZPA6_PBAHp1siSUQeBIiJE/view?usp=sharing"
-              target="_black"
+  render() {
+    let width = window.innerWidth;
+    if (width > 700) {
+      return (
+        <div className="demo-big-content">
+          <Layout>
+            <Header
+              className="header-color"
+              title={
+                <Link style={{ textDecoration: "none", color: "white" }} to="/">
+                  Profile{" "}
+                </Link>
+              }
+              scroll
             >
-              Resume
-            </a>
-            
-            <Link to="/skills">Skills</Link>
-            <Link to="/projects">Projects</Link>
-            <Link to="/contact">Contact</Link>
-          </Navigation>
-        </Header>
+              <Navigation>
+                <Link to="/aboutme">About me</Link>
+                <a
+                  href="https://drive.google.com/file/d/1jYPWM_KJPWZPA6_PBAHp1siSUQeBIiJE/view?usp=sharing"
+                  target="_black"
+                >
+                  Resume
+                </a>
 
-        <Content>
-          <div className="page-content" />
-          <Main />
-        </Content>
-        
-                    </Layout>
+                <Link to="/skills">Skills</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
+              </Navigation>
+            </Header>
 
-
-                </div>
-
-
-         
-         );
-        }else {
-            return(
-                <div className="demo-big-content">
-                <Layout>
-                <Drawer
-          title={
-            <Link style={{ textDecoration: "none", color: "Black" }} to="/">
-              Profile{" "}
-            </Link>
-          }
-        >
-          <Navigation>
-          <Link to="/aboutme">About me</Link>
-          <a
-              href="https://drive.google.com/file/d/1jYPWM_KJPWZPA6_PBAHp1siSUQeBIiJE/view?usp=sharing"
-              target="_black"
-            >
-              Resume
-            </a>
-            
-            <Link to="/skills">Skills</Link>
-            <Link to="/projects">Projects</Link>
-            <Link to="/contact">Contact</Link>
-          </Navigation>
-        </Drawer> 
-
-        <Content>
-          <div className="page-content" />
-          <Main />
-        </Content>
-        </Layout>
+            <Content>
+              <div className="page-content" />
+              <Main />
+            </Content>
+          </Layout>
         </div>
-            )
-        }
+      );
+    } else {
+      return (
+        <div className="demo-big-content">
+          <Layout>
+            <Drawer
+              title={
+                <Link style={{ textDecoration: "none", color: "Black" }} to="/">
+                  Profile{" "}
+                </Link>
+              }
+            >
+              <Navigation>
+                <Link to="/aboutme">About me</Link>
+                <a
+                  href="https://drive.google.com/file/d/1jYPWM_KJPWZPA6_PBAHp1siSUQeBIiJE/view?usp=sharing"
+                  target="_black"
+                >
+                  Resume
+                </a>
+
+                <Link to="/skills">Skills</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
+              </Navigation>
+            </Drawer>
+
+            <Content>
+              <div className="page-content" />
+              <Main />
+            </Content>
+          </Layout>
+        </div>
+      );
     }
+  }
 }
- 
+
 export default NavComponent;
